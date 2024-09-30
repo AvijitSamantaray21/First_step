8dbfeeae49681d1e54bdd56c0f54d3bf32d15ed4
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-09-30 12:06:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,10 +12,10 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id            = var.client_id  # Can also be set via `ARM_CLIENT_ID` environment variable.
+     client_id           = var.client_id  # Can also be set via `ARM_CLIENT_ID` environment variable.
     subscription_id      = var.subscription_id  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
     tenant_id            = var.tenant_id  # Can also be set via `ARM_TENANT_ID` environment variable.
-    client_secret = var.client_secret
+    client_secret        =  var.client_secret
  
 }
 
