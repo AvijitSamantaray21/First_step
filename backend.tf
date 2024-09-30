3b1f804471d91d9d67fb1ea4c6e460edde4fc68b
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-09-30 18:50:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,6 +5,10 @@ terraform {
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
     use_msi              = true                                    # Can also be set via `ARM_USE_MSI` environment variable.
+    client_id            = "client_id_value" 
+    subscription_id      = "subsription_id_value"  
+    tenant_id            = "tenant_id_value" 
+    client_secret        ="client_secret_value"
    
    
   }
