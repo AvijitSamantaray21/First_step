2435d4340cd3cb8127fc5618da95b44a3164e13c
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-09-30 12:23:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,9 +5,9 @@ terraform {
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
     use_msi              = true                                    # Can also be set via `ARM_USE_MSI` environment variable.
-    client_id            = var.client_id  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    subscription_id      = var.subscription_id  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-    tenant_id            = var.tenant_id  # Can also be set via `ARM_TENANT_ID` environment variable.
-    client_secret = var.client_secret
+    client_id            = "${client_id}"  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    subscription_id      = "${subscription_id}"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    tenant_id            = "${tenant_id}"  # Can also be set via `ARM_TENANT_ID` environment variable.
+    client_secret = "${client_secret}"
   }
 }
