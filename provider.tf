5f10bed6c7db80cf613fa02037a829cf4ef02f68
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-09-30 12:02:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,9 @@
 terraform {
   required_providers {
- 
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
     github = {
       source = "integrations/github"
       version = "6.3.0"
@@ -8,6 +11,13 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  client_id            = var.client_id  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    subscription_id      = var.subscription_id  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    tenant_id            = var.tenant_id  # Can also be set via `ARM_TENANT_ID` environment variable.
+    client_secret = var.client_secret
+ 
+}
 
 
 
