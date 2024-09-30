e6682c75c11ed5e450c153b6865a3192d975ed96
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-09-30 17:15:56 +0530

    Update main.tf

--- create_backend/main.tf
+++ create_backend/main.tf
@@ -7,8 +7,10 @@ resource "azurerm_storage_account" "example" {
   name                     = "tfmasterhi"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
+    account_tier             = "Standard"
   account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
   depends_on = [ azurerm_resource_group.example ]
 
 }
