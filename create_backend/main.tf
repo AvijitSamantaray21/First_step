e527b58d17e5259449399d727661a812e6d90628
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-01 09:40:54 +0530

    Update main.tf

--- create_backend/main.tf
+++ create_backend/main.tf
@@ -1,7 +1,7 @@
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "tfmasterhi-testing"
+  name                     = "storagetfTesting"
   resource_group_name      = "hihi"
   location                 = "centralindia"
   account_tier             = "Standard"
@@ -13,7 +13,7 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_storage_container" "example" {
-  name                  = "tfstate-testing"
+  name                  = "tfstateTesting"
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
   depends_on = [azurerm_storage_account.example ]
