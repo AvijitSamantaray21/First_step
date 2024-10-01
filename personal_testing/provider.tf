e7c276090920b8ee564633cd2c6974e799d5774d
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-01 10:34:55 +0530

    Create provider.tf

---
+++ personal_testing/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+      azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.3.0"
+     }
+
+provider "azurerm" {
+  storage_use_azuread = true
+  features {}
+}
