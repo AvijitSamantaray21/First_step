3038796deeeb83e8e3caad8452d983cabf4b155c
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-01 10:47:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,12 +4,12 @@ terraform {
     storage_account_name = "tfmasterhi"                              
     container_name       = "tfstate"                              
     key                  = "terraform.tfstate"               
-    use_msi              = true                                    
+    use_oidc             = true                                  
     client_id            = "client_id_value" 
     subscription_id      = "subsription_id_value"  
     tenant_id            = "tenant_id_value"
-    client_secret        ="client_secret_value"
-    use_azuread_auth     = true
+    
+   
    
    
    
