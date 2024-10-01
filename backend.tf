4d8db58a64c0239cb8adb3eeb58f66322248a6b1
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-01 11:13:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,9 +5,9 @@ terraform {
     container_name       = "tfstate"                              
     key                  = "terraform.tfstate"               
     use_oidc             = true                                  
-    client_id            = "client_id_value" 
-    subscription_id      = "subsription_id_value"  
-    tenant_id            = "tenant_id_value"
+    client_id            = "?" 
+    subscription_id      = "?"  
+    tenant_id            = "?"
     
    
    
