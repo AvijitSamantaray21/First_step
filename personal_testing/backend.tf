5b489754ee512b855c68d2e70b5f20bc13cb22e4
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-01 09:08:15 +0530

    Update backend.tf

--- personal_testing/backend.tf
+++ personal_testing/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "azurerm" {
-    
- 
-   
-   
-  }
-}
+    container_name = "tfstate"
+    key = "terraform_testing.tfstate"
+    storage_account_name = "tfmasterhi"
+    }
+    }
+
+
+
