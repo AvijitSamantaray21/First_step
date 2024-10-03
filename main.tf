c098fd1b75cb6a7d565237cda373938b0f8ca38c
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-03 19:58:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "azurerm_client_config" "current" {
 }
 
 output "client_id" {
-  value = data.azurerm_client_config.current.client_id
+  value = data.azurerm_client_config.current.object_id
 }
 
 output "tenant_id" {
