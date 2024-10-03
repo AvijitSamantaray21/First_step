83fdb0ec16940490c08f6381e2e6abd890b0d9bd
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-03 19:04:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,6 +16,9 @@ terraform {
 
 provider "azurerm" {
   storage_use_azuread = true
+  client_id            = var.client_id
+  subscription_id      = var.subscription_id
+  tenant_id            = var_tenant_id
   features {}
 }
 provider "github" {
@@ -25,4 +28,16 @@ provider "github" {
 variable "github_token" {
   description = "GitHub Personal Access Token"
   type        = string
+}
+
+variable "client_id"{
+  type        = string
+}
+
+variable "subscription_id {
+v
+}
+
+variable " tenant_id "{
+  type        =  string
 }
