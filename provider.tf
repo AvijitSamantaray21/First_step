b31ec9be0012f25bf8bae841052614b164e41e48
AvijitSamantaray21 <avijit.samantaray2@cognizant.com>
2024-10-03 19:47:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,24 +1,22 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.3.0"
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
-    github = {
+     github = {
       source = "integrations/github"
       version = "6.3.0"
     }
-  }
+  }    
 }
-
-
-
-
 provider "azurerm" {
-  
   storage_use_azuread = true
   features {}
 }
+
+
+
 provider "github" {
   token = var.github_token
 }
